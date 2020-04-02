a7b69123279d0e846f5956b719235a57577f0f34
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2020-04-02 02:40:31 -0300

    tentei

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+
+import { View, Text, TextInput, Image, TouchableOpacity } from 'react-native';
 
 import { FontAwesome } from '@expo/vector-icons'
+
+import Pokeball from './Assets/pokeball.png'
 
 import styles from './styles';
 
 export default function Home() {
   return (
     <View style={styles.container}>
+        <Image source={Pokeball} style={styles.background}/>
       <View style={styles.content}>
         <Text style={styles.title}>What Pokemon</Text>
         <Text style={styles.title}>are you looking for?</Text>
 
         <View style={styles.searchContainer}>
-          <FontAwesome name='search' color="#000" size={20} />
+          <FontAwesome name='search' color="#000" size={20} style={{marginLeft: 5}} />
           <TextInput style={{width: '100%', marginLeft: 20}} placeholder='Search Pokemon, Move, Ability etc.' />
         </View>
+        
+      <View style={styles.menu}>
+        <TouchableOpacity style={styles.card}>
+            <Text style={styles.cardText}>Pokedex</Text>
+          <Image source={Pokeball} style={styles.backgroundCard2}/>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+            <Text style={styles.cardText}>Moves</Text>
+          <Image source={Pokeball} style={styles.backgroundCard2}/>
+        </TouchableOpacity>
+
+      </View>
+
+      <View style={styles.menu2}>
+        <TouchableOpacity style={styles.card}>
+            <Text style={styles.cardText}>Abilities</Text>
+          <Image source={Pokeball} style={styles.backgroundCard2}/>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+          <Text style={styles.cardText}>Items</Text>
+          <Image source={Pokeball} style={styles.backgroundCard2}/>
+        </TouchableOpacity>
+      </View>
+       
 
 
       </View>
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -8,14 +8,22 @@ const styles = StyleSheet.create({
         flex: 1,
     },
 
+    background: {
+        position: 'absolute',
+        width: 350,
+        height: 350,
+        right: -123,
+        top: -90
+    },
+
     content: {
-        marginTop: Constants.statusBarHeight + 40,
+        marginTop: Constants.statusBarHeight + 170,
         marginHorizontal: 20
     },
 
     title: {
         fontWeight: 'bold',
-        fontSize: 35,
+        fontSize: 40,
     },
     searchContainer: {
         backgroundColor: '#F6F6F6',
@@ -24,6 +32,56 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 25,
         alignItems: 'center'
+    },
+
+    menu: {
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: "space-evenly"
+    },
+
+    menu2: {
+        marginTop: 5,
+        flexDirection: 'row',
+        justifyContent: "space-evenly"
+    },
+    card: {
+        backgroundColor: "#4EC2A6",
+        width: 180,
+        height: 100,
+        height: 70,
+        padding: 20,
+        borderRadius: 30,
+        flexDirection: 'row',
+        margin: 10
+    },
+
+    backgroundCard1: {
+        opacity: 0.2,
+        backgroundColor: '#F6F6F6',
+        position: 'absolute',
+        width: 60,
+        height: 60,
+        top: -25,
+        left: -25, 
+        borderRadius: 30
+    },
+
+    cardText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 25,
+        justifyContent: 'center',
+    },
+
+    backgroundCard2: {
+        opacity: 0.2,
+        width: 60,
+        height: 60,
+        position: 'absolute',
+        right: 2,
+        marginTop: 5,
+        marginLeft: 10,
     }
 })
 
